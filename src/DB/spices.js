df68dbcb136e9c3d0c00d351aa56e552d61cf21b
Provash Shoumma <provash.shoumma@gmail.com>
2018-04-02 11:48:12 +0600

    updated spice DB

--- src/DB/spices.js
+++ src/DB/spices.js
@@ -58,5 +58,304 @@ export default [
       { source: require('./assets/Cardamom/2.jpg') },
       { source: require('./assets/Cardamom/3.jpg') },
     ]
-  }
+  },
+  {
+    id: 6,
+    name: 'Celery Seeds',
+    description: 'Celery seeds, the seed of a plant of the parsley family related to the celery plant, with a celery like flavor and aroma.',
+    usage: 'They can be used to boost the flavor of a dish and lend it an aromatic twist.',
+    thumbnail: require('./assets/Celery_Seeds/1.jpg'),
+    images: [
+      { source: require('./assets/Celery_Seeds/1.jpg') },
+      { source: require('./assets/Celery_Seeds/2.jpg') },
+      { source: require('./assets/Celery_Seeds/3.jpg') },
+    ]
+  },
+  {
+    id: 7,
+    name: 'Chile Pepper',
+    description: 'Chile pepper is a small hot-tasting pod of a variety of capsicum, used chopped and often dried in sauces, relished, and spice powders. There are various kinds with pods of differing size, color, and strength of flavor.',
+    usage: 'They are used as powder, sauce, paste, or whole, fresh or dried. They are used for making pickles, cooking, roasting or all kings of spicy foods. It can also be eaten raw.',
+    thumbnail: require('./assets/Chile_Pepper/1.jpg'),
+    images: [
+      { source: require('./assets/Chile_Pepper/1.jpg') },
+      { source: require('./assets/Chile_Pepper/2.jpg') },
+      { source: require('./assets/Chile_Pepper/3.jpg') },
+    ]
+  },
+  {
+    id: 8,
+    name: 'Cinnamon',
+    description: 'Cinnamon, an aromatic spice made from the peeled, dried, and rolled bark of a Southeast Asian tree.',
+    usage: 'It is used as an aromatic condiment and flavoring additive in a wide variety of cuisines, sweet and savoury dishes, breakfast cereals, snack foods, and traditional foods.',
+    thumbnail: require('./assets/Cinnamon/1.jpg'),
+    images: [
+      { source: require('./assets/Cinnamon/1.jpg') },
+      { source: require('./assets/Cinnamon/2.jpg') },
+      { source: require('./assets/Cinnamon/3.jpg') },
+    ]
+  },
+  {
+    id: 9,
+    name: 'Clove',
+    description: 'Clove, the dried flower bud of a tropical tree, used as a pungent aromatic spice.',
+    usage: 'Whole cloves are used in cooking, potpourri and other crafts. The ground spice is primarily used in baking and can also be combined with arrowroots for use as a natural blusher or foundation. Clove tea is a good option for a flavourful, nutritious hot beverage.',
+    thumbnail: require('./assets/Clove/1.jpg'),
+    images: [
+      { source: require('./assets/Clove/1.jpg') },
+      { source: require('./assets/Clove/2.jpg') },
+      { source: require('./assets/Clove/3.jpg') },
+    ]
+  },
+  {
+    id: 10,
+    name: 'Coriander',
+    description: 'Coriander, an aromatic Mediterranean plant of the parsley family, the leaves and seeds of which are used as culinary herbs.',
+    usage: 'Coriander leaves are used for decorating dishes, salad, and Indian curries. Coriander seeds are used as powder for making curries or vegetables.',
+    thumbnail: require('./assets/Coriander/1.jpg'),
+    images: [
+      { source: require('./assets/Coriander/1.jpg') },
+      { source: require('./assets/Coriander/2.jpg') },
+      { source: require('./assets/Coriander/3.jpg') },
+    ]
+  },
+  {
+    id: 11,
+    name: 'Dill',
+    description: 'Dill, an aromatic annual herb of the parsley family, with fine blue-green leaves and yellow flowers. The leaves and seeds of dill are used for flavoring and for medicinal purposes.',
+    usage: 'It is used as an ingredient in pickling seasoning and to flavor breads, cheese, meats and vegetables.',
+    thumbnail: require('./assets/Dill/1.jpg'),
+    images: [
+      { source: require('./assets/Dill/1.jpg') },
+      { source: require('./assets/Dill/2.jpg') },
+    ]
+  },
+  {
+    id: 12,
+    name: 'Fenugreek',
+    description: 'Fenugreek, a white-flowered herbaceous plant of the pea family, with aromatic seeds that are used for flavoring, especially ground and used in curry powder.',
+    usage: 'Indian Curries, pita bread, meat etc.',
+    thumbnail: require('./assets/Fenugreek/1.jpg'),
+    images: [
+      { source: require('./assets/Fenugreek/1.jpg') },
+      { source: require('./assets/Fenugreek/2.jpg') },
+      { source: require('./assets/Fenugreek/3.jpg') },
+    ]
+  },
+  {
+    id: 13,
+    name: 'Frankincense',
+    description: 'Frankincense, an aromatic gum resin obtained from an African tree and burned as incense.',
+    usage: 'Used for its perfume in desserts or sweets.',
+    thumbnail: require('./assets/Frankincense/1.jpg'),
+    images: [
+      { source: require('./assets/Frankincense/1.jpg') },
+      { source: require('./assets/Frankincense/2.jpg') },
+      { source: require('./assets/Frankincense/3.jpg') },
+    ]
+  },
+  {
+    id: 14,
+    name: 'Galangal',
+    description: 'Galangal, an Asian plant of the ginger family, the aromatic rhizome of which is widely used in cooking and herbal medicine.',
+    usage: 'Used in traditional Asian cuisines, such as Thai and Lao tom yam and tom kha gai soups. Some vodkas are flavoured with galangal.',
+    thumbnail: require('./assets/Galangal/1.jpg'),
+    images: [
+      { source: require('./assets/Galangal/1.jpg') },
+      { source: require('./assets/Galangal/2.jpg') },
+      { source: require('./assets/Galangal/3.jpg') },
+    ]
+  },
+  {
+    id: 15,
+    name: 'Garlic',
+    description: 'Garlic, a strong-smelling pungent-tasting bulb, used as a flavoring in cooking and in herbal medicine.',
+    usage: 'It is a fundamental component in many or most dishes. It is a fragrant and test booster. Vegetables, meats, breads, pasta, fish sauce and so on.',
+    thumbnail: require('./assets/Garlic/1.jpg'),
+    images: [
+      { source: require('./assets/Garlic/1.jpg') },
+      { source: require('./assets/Garlic/2.jpg') },
+      { source: require('./assets/Garlic/3.jpg') },
+    ]
+  },
+  {
+    id: 16,
+    name: 'Ghost Pepper',
+    description: 'Ghost pepper, a variety of chili pepper grown in South Asia that is one of the hottest forms known. It is also known as Bhut jolokia.',
+    usage: 'Used for hot test, in burger, cheese, sauces etc.',
+    thumbnail: require('./assets/Ghost_Pepper/1.jpg'),
+    images: [
+      { source: require('./assets/Ghost_Pepper/1.jpg') },
+      { source: require('./assets/Ghost_Pepper/2.jpg') },
+      { source: require('./assets/Ghost_Pepper/3.jpg') },
+    ]
+  },
+  {
+    id: 17,
+    name: 'Ginger',
+    description: 'Ginger, a hot fragrant spice made from the rhizome of a plant. It is chopped or powdered for cooking, preserved in syrup, or candied.',
+    usage: 'Ginger is a popular spice with both sweet and savory applications. Ginger tea is also famous, testful and nutritious.',
+    thumbnail: require('./assets/Ginger/1.jpg'),
+    images: [
+      { source: require('./assets/Ginger/1.jpg') },
+      { source: require('./assets/Ginger/2.jpg') },
+      { source: require('./assets/Ginger/3.jpg') },
+    ]
+  },
+  {
+    id: 18,
+    name: 'Green Chili Pepper',
+    description: 'Green chili pepper, the green chili group includes all green peppers that are hot, including “Anaheim”, “Jalapeno”, “Cayenne”.',
+    usage: 'Any type of curry, salad, fresh or roasted.',
+    thumbnail: require('./assets/Green_Chili/1.jpg'),
+    images: [
+      { source: require('./assets/Green_Chili/1.jpg') },
+      { source: require('./assets/Green_Chili/2.jpg') },
+      { source: require('./assets/Green_Chili/3.png') },
+    ]
+  },
+  {
+    id: 19,
+    name: 'Horseradish',
+    description: 'Horseradish, a European plant of the cabbage family, with long docklike leaves, grown for its pungent root.',
+    usage: 'Used as sauces or pastes, specially on sandwiches.',
+    thumbnail: require('./assets/Horseradish/1.jpg'),
+    images: [
+      { source: require('./assets/Horseradish/1.jpg') },
+      { source: require('./assets/Horseradish/2.jpg') },
+      { source: require('./assets/Horseradish/3.jpg') },
+    ]
+  },
+  {
+    id: 20,
+    name: 'Mace',
+    description: 'Mace, the reddish fleshy outer covering of the nutmeg, dried as a spice.',
+    usage: 'Some uses it to flavor meat and seafood dishes; others use it to season fruits and desserts, like cookies and rice pudding.',
+    thumbnail: require('./assets/Mace/1.jpg'),
+    images: [
+      { source: require('./assets/Mace/1.jpg') },
+      { source: require('./assets/Mace/2.jpg') },
+      { source: require('./assets/Mace/3.jpg') },
+    ]
+  },
+  {
+    id: 21,
+    name: 'Mustard',
+    description: 'Mustard, a pungent-tasting yellow or brown paste made from the crushed seeds of certain plants, typically eaten with meat or used as a cooking ingredient.',
+    usage: 'It can be used as oil and sauce or paste. Commonly itt paired with meats and cheeses, also in sandwiches, hamburgers, corn dogs, hot dogs, soups. Indians use it in curries, fishes and vegetables.',
+    thumbnail: require('./assets/Mustard/1.jpg'),
+    images: [
+      { source: require('./assets/Mustard/1.jpg') },
+      { source: require('./assets/Mustard/2.jpg') },
+      { source: require('./assets/Mustard/3.jpg') },
+    ]
+  },
+  {
+    id: 22,
+    name: 'Nutmeg',
+    description: 'Nutmeg, the hard, aromatic, almost spherical seed of a tropical tree.',
+    usage: 'It is used to flavour many kinds of baked goods, confections, puddings, potatoes, meats, sausages, sauces, vegetables, and such beverages as eggnog.',
+    thumbnail: require('./assets/Nutmeg/1.jpg'),
+    images: [
+      { source: require('./assets/Nutmeg/1.jpg') },
+      { source: require('./assets/Nutmeg/2.jpg') },
+      { source: require('./assets/Nutmeg/3.jpg') },
+    ]
+  },
+  {
+    id: 23,
+    name: 'Onion',
+    description: 'Onion, an edible bulb with a pungent taste and smell, composed of several concentric layers, used in cooking.',
+    usage: 'Onions are commonly chopped and used as an ingredient in various hearty warm dishes, and may also be used as a main ingredient in their own right, for example in French onion soup, creamed onions, and onion chutney. They are versatile and can be baked, boiled, braised, grilled, fried, roasted, sautéed, or eaten raw in salads, curries and gravies.',
+    thumbnail: require('./assets/Onion/1.jpg'),
+    images: [
+      { source: require('./assets/Onion/1.jpg') },
+      { source: require('./assets/Onion/2.jpg') },
+      { source: require('./assets/Onion/3.jpg') },
+    ]
+  },
+  {
+    id: 24,
+    name: 'Peppercorns',
+    description: 'Peppercorns, the dried berry of a climbing vine, used whole as a spice or crushed or ground to make pepper.',
+    usage: 'Lightly crushed or cracked peppercorns can be used to spice up creamy sauces or to coat fillet steaks or chicken breasts. Whole or ground peppercorns are used in soup or curries.',
+    thumbnail: require('./assets/Peppercorns/1.jpg'),
+    images: [
+      { source: require('./assets/Peppercorns/1.jpg') },
+      { source: require('./assets/Peppercorns/2.jpg') },
+      { source: require('./assets/Peppercorns/3.jpg') },
+    ]
+  },
+  {
+    id: 25,
+    name: 'Savory',
+    description: 'Savory, an aromatic plant of the mint family, used as a culinary herb.',
+    usage: 'This fragrant spice mix adds a festive winter vibe to roasted sweet potatoes or squash and is a perfect addition to chewy whole grain dishes.',
+    thumbnail: require('./assets/Savory/1.jpg'),
+    images: [
+      { source: require('./assets/Savory/1.jpg') },
+      { source: require('./assets/Savory/2.jpg') },
+      { source: require('./assets/Savory/3.jpg') },
+    ]
+  },
+  {
+    id: 26,
+    name: 'Sesame',
+    description: 'Sesame, a tall annual herbaceous plant of tropical and subtropical areas of the Old World, cultivated for its oil-rich seeds.',
+    usage: 'It has rich, nutty flavor that is used in breads, burger bans, bagels, crackers, sticks, cakes etc.',
+    thumbnail: require('./assets/Seasame/1.jpg'),
+    images: [
+      { source: require('./assets/Seasame/1.jpg') },
+      { source: require('./assets/Seasame/2.jpg') },
+      { source: require('./assets/Seasame/3.jpg') },
+    ]
+  },
+  {
+    id: 27,
+    name: 'Sumac',
+    description: 'Sumac, a shrub or small tree of the cashew family, with compound leaves, fruits in conical clusters, and bright autumn colors.',
+    usage: 'Sumac is a widely used, essential spice in Middle Eastern and Mediterranean cooking. It\'s used in everything from dry rubs, marinades, and dressing. But its best use is sprinkled over food before serving. It pairs well with vegetables, grilled lamb, chicken and fish. Sumac is used as a topping on fattoush salad, and makes a nice topping on dips like hummus.',
+    thumbnail: require('./assets/Sumac/1.jpg'),
+    images: [
+      { source: require('./assets/Sumac/1.jpg') },
+      { source: require('./assets/Sumac/2.jpg') },
+      { source: require('./assets/Sumac/3.jpg') },
+    ]
+  },
+  {
+    id: 28,
+    name: 'Tabasco Pepper',
+    description: 'Tabasco pepper, is a variety of chili pepper species Capsicum frutescens with its origins in Mexico.',
+    usage: 'It is best known through its use in Tabasco sauce, followed by peppered vinegar.',
+    thumbnail: require('./assets/Tabasco_Pepper/1.jpg'),
+    images: [
+      { source: require('./assets/Tabasco_Pepper/1.jpg') },
+      { source: require('./assets/Tabasco_Pepper/2.jpg') },
+      { source: require('./assets/Tabasco_Pepper/3.jpg') },
+    ]
+  },
+  {
+    id: 29,
+    name: 'Tamarind',
+    description: 'Tamarind, sticky brown acidic pulp from the pod of a tree of the pea family, widely used as a flavoring in Asian cooking.  Tamarind powder is used as a condiment and souring agent.',
+    usage: 'Tamarind is commonly used in soups, sauces, curries and poultry, fish, lentil and rice dishes. It also features prominently in fruit drinks. Tamarind also is a great "secret ingredient" for your homemade barbecue sauce, hot sauces, and marinades.',
+    thumbnail: require('./assets/Tamarind/1.jpg'),
+    images: [
+      { source: require('./assets/Tamarind/1.jpg') },
+      { source: require('./assets/Tamarind/2.jpg') },
+      { source: require('./assets/Tamarind/3.jpg') },
+    ]
+  },
+  {
+    id: 30,
+    name: 'Turmeric',
+    description: 'Turmeric, a bright yellow aromatic powder obtained from the rhizome of a plant of the ginger family, used for flavoring and coloring in Asian cooking and formerly as a fabric dye.',
+    usage: 'Commonly used as a coloring and flavoring agent in many Asian cuisines, especially for curries, as well as for dyeing.',
+    thumbnail: require('./assets/Tumeric/1.jpg'),
+    images: [
+      { source: require('./assets/Tumeric/1.jpg') },
+      { source: require('./assets/Tumeric/2.jpg') },
+      { source: require('./assets/Tumeric/3.jpg') },
+    ]
+  },
 ]
